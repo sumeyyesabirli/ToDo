27d7e3a43374b7d928cd6505522ca65b289351f6
example <sumeyyesabirli@gmail.com>
2023-05-07 00:53:42 +0300

    Register User password hash

--- Todo.Application/Services/Commands/User/Register/RegisterUserCommandHandler.cs
+++ Todo.Application/Services/Commands/User/Register/RegisterUserCommandHandler.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Todo.Application.Repositories;
+using Todo.Core;
 
 namespace Todo.Application.Services.Commands.User.Insert
 {
@@ -22,7 +23,10 @@ namespace Todo.Application.Services.Commands.User.Insert
 
         public async Task<bool> Handle(RegisterUserCommandRequest request, CancellationToken cancellationToken)
         {
+            
+            string hashedPassword = PasswordEncryptor.Encrpt(request.Password);         
             var map = _mapper.Map<Core.Entities.User>(request);
+            map.Password = hashedPassword;            
             var userRepo = await _userRepository.AddAsync(map);
             await _userRepository.SaveAsync();
             return true;
